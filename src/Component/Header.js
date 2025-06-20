04a351b7b7a6fc9c2bc35d2bbc5324bd92dd090a
monu-sharma0820 <sharmasrishti0820@gmail.com>
2025-06-21 00:54:11 +0530

    minor changes done

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 function Header() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <Link class="navbar-brand" to="#">
             Navbar
-          </a>
+          </Link>
           <button
             class="navbar-toggler"
             type="button"
@@ -22,50 +23,50 @@ function Header() {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
+                <Link class="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li class="nav-item">
-                 <a class="nav-link active" aria-current="page" href="/game">
+                 <Link class="nav-link active" aria-current="page" to="/game">
                    7UP & 7Down
-                </a>
+                </Link>
               </li>
               <li class="nav-item dropdown">
-                <a
+                <Link
                   class="nav-link dropdown-toggle"
-                  href="#"
+                  to="#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   Dropdown
-                </a>
+                </Link>
                 <ul class="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <Link class="dropdown-item" to="#">
                       Action
-                    </a>
+                    </Link>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <Link class="dropdown-item" to="#">
                       Another action
-                    </a>
+                    </Link>
                   </li>
                   <li>
                     <hr class="dropdown-divider" />
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">
+                    <Link class="dropdown-item" to="#">
                       Something else here
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">
+                <Link class="nav-link disabled" aria-disabled="true">
                   Disabled
-                </a>
+                </Link>
               </li>
             </ul>
             <form class="d-flex" role="search">
